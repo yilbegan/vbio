5d96d21d5827df69d6f3aafd65337cfaeb62abf9
yilbegan <w2v@protonmail.com>
2019-01-09 19:54:05 +0300

    conf.py fix.

--- docs/conf.py
+++ docs/conf.py
@@ -14,13 +14,13 @@
 
 import os
 import sys
-import vbio
 
 from datetime import datetime
 
 
 sys.path.insert(0, os.path.abspath('../'))
 
+import vbio
 
 # -- Project information -----------------------------------------------------
 
