de10537c34abfc18c0334916f8e769e55e2a743c
eugen1337 <es.chetvertkov@ok2083.ru>
2019-01-09 15:23:11 +0300

    On branch master
    Changes to be committed:
    	modified:   setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,16 @@ import vbio
 
 setuptools.setup(
     name="vbio",
+
     version=vbio.__version__,
     author=vbio.__author__,
+
+    description=u'Python модуль для написания скриптов, использующих '
+                u'Callback API для социальной сети '
+                u'Вконтакте (vk.com)',
+
+    long_description=open('README.md', encoding='utf-8').read(),
+    long_description_content_type='text/markdown',
+    
     packages=['vbio', 'vbio.servers']
 )
