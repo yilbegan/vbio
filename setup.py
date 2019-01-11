8431e9f2ace2d8fa964d0259c6ae32581a6eb32b
yilbegan <w2v@protonmail.com>
2019-01-11 20:21:20 +0300

    setup.py update

--- setup.py
+++ setup.py
@@ -21,7 +21,25 @@ setuptools.setup(
     long_description_content_type='text/markdown',
 
     packages=['vbio', 'vbio.servers'],
-    install_requires=['Flask',
-                      'CherryPy',
-                      'requests']
+    install_requires=['requests'],
+    extras_require={
+        'flask_webhook': ['flask'],
+        'cherrypy_webhook': ['cherrypy'],
+    },
+
+
+    classifiers=[
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Programming Language :: Python :: Implementation :: CPython',
+    ]
 )
